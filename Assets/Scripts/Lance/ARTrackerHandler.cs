283b271c02de16684848f63c5fccd30004826f74
uwu-owo-o3o <lance_d_ong@dlsu.edu.ph>
2024-12-05 03:39:59 +0800

    confused a bit about tasks...

--- Assets/Scripts/Lance/ARTrackerHandler.cs
+++ Assets/Scripts/Lance/ARTrackerHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.UI;
+using LilLycanLord_Official;
 
 public class TrackHandler : MonoBehaviour
 {
@@ -41,41 +42,55 @@ public class TrackHandler : MonoBehaviour
         if (name.Contains("kitchen"))
         {
             this.debugText.text = "kitchen scanned!";
+            this.callTaskManagerForTask();
         }
         else if (name.Contains("dining"))
         {
             this.debugText.text = "dining scanned!";
+            this.callTaskManagerForTask();
         }
         else if (name.Contains("guesthouse"))
         {
             this.debugText.text = "guesthouse scanned!";
+            this.callTaskManagerForTask();
         }
         else if (name.Contains("patio"))
         {
             this.debugText.text = "patio scanned!";
+            this.callTaskManagerForTask();
         }
         else if (name.Contains("spa"))
         {
             this.debugText.text = "spa scanned!";
+            this.callTaskManagerForTask();
         }
         else if (name.Contains("theatre"))
         {
             this.debugText.text = "theatre scanned!";
+            this.callTaskManagerForTask();
         }
         else if (name.Contains("living"))
         {
             this.debugText.text = "living scanned!";
+            this.callTaskManagerForTask();
         }
         else if (name.Contains("observatory"))
         {
             this.debugText.text = "observatory scanned!";
+            this.callTaskManagerForTask();
         }
         else if (name.Contains("cat"))
         {
             this.debugText.text = "cat scanned!";
+            this.callTaskManagerForTask();
         }
 
         this.debugText.text = "NONE";
 
+    }
+
+    public void callTaskManagerForTask()
+    {
+        TaskManager.Instance.GetTask();
     }
 }
---
+++ Assets/Scripts/Lance/GoHome.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using LilLycanLord_Official;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoHome : MonoBehaviour
+{   
+    public void goBack()
+    {
+        SceneManager.LoadScene("UI");
+    }
+    
+    public void debugGetTask()
+    {
+        TaskManager.Instance.GetTask();
+    }
+}
