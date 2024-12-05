49e03c14e9694e63c8a86d3e05a315cf30178058
DerekColbeeUy <derek_colbee_uy@dlsu.edu.ph>
2024-12-05 15:28:05 +0800

    ok

--- Assets/Scripts/DetectiveNotebook.cs
+++ Assets/Scripts/DetectiveNotebook.cs
@@ -1,10 +1,20 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DetectiveNotebook : MonoBehaviour
 {
     private void Awake()
     {
+
         DontDestroyOnLoad(gameObject);
 
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Detective Scene")
+        {
+            this.gameObject.SetActive(true);
+        }
     }
 }
